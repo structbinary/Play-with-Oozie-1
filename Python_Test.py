c3d637e903771951a086fcee5897549e1ad4b4d8
Ankit Shukla <ashukla@XEBIAINDIA.LOCAL>
2019-06-13 16:07:09 +0530

    demo file 13-6-2019

---
+++ Python_Test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+import sys
+import os
+
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
+import os
+print os.environ["Pig"]
+
+
+
