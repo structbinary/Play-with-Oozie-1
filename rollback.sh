1db8f0a4fd527ffc364056e1297ff5f5a262360c
Ankit Shukla <ashukla@XEBIAINDIA.LOCAL>
2019-06-17 19:16:44 +0530

    rollback script change

--- rollback.sh
+++ rollback.sh
@@ -1,63 +1,125 @@
 #!/bin/bash
 
+ 
+
 successflag=$1
+
 echo "Flag is set to:" $successflag
 
+basepath=/user/cloudera/examples/apps
+
+tmppath=/user/cloudera/examples/apps/tmp-java
+
+sourcepath=/user/cloudera/examples/apps/java-main
+
+localpath=/home/cloudera/examples/apps/java-main
+
+ 
 
 list_hdfs () {
-  hdfs dfs -ls /user/cloudera/examples/apps
+
+  hdfs dfs -ls $basepath
+
 }
+
+ 
 
 check_dir_exists () {
 
-if hdfs dfs -test -d /user/cloudera/examples/apps/tmp-java ; then
-	echo "Directory  exists"
-	hdfs dfs -rm -r /user/cloudera/examples/apps/tmp-java
-	echo "Deleting directory"
-	list_hdfs
+ 
+
+if hdfs dfs -test -d $tmppath ; then
+
+    echo "Directory  exists"
+
+    hdfs dfs -rm -r $tmppath
+
+    echo "Deleting directory"
+
+    list_hdfs
+
 else
-	hdfs dfs -mkdir /user/cloudera/examples/apps/tmp-java
-	echo "Creating  directory"
-	list_hdfs
+
+    create_tmp_folder
+
+    echo "Creating  directory"
+
+    
+
 fi
 
+ 
+
 }
+
+ 
 
 create_tmp_folder () {
-hdfs dfs -mkdir /user/cloudera/examples/apps/tmp-java
+
+hdfs dfs -mkdir $tmppath
+
 list_hdfs
+
 }
+
+ 
 
 move_from_tmp () {
-hdfs dfs -mv /user/cloudera/examples/apps/java-main /user/cloudera/examples/apps/tmp-java
+
+hdfs dfs -mv $1 $2
+
 list_hdfs
+
 }
+
+ 
 
 copy_from_local () {
-hdfs dfs -copyFromLocal /home/cloudera/examples/apps/java-main /user/cloudera/examples/apps/
+
+hdfs dfs -copyFromLocal $localpath $basepath
+
 list_hdfs
+
 }
 
+ 
+
 echo "Print the directory structure"
+
 list_hdfs
 
-echo "Remove the tmp folder"
+ 
+
+echo "checking directory exists or not"
+
 check_dir_exists
 
+ 
+
 echo "Create tmp folder"
+
 create_tmp_folder
 
+ 
+
 echo "Copy to tmp location"
-move_from_tmp
+
+move_from_tmp $sourcepath $tmppath
+
+ 
 
 if [ $successflag -eq 1 ];
-then 
-	echo "Build Success"
-	copy_from_local
+
+then
+
+    echo "Build Success"
+
+    copy_from_local
+
 else
-	echo "Build Failed"
-	hdfs dfs -mv /user/cloudera/examples/apps/tmp-java /user/cloudera/examples/apps/java-main
+
+    echo "Build Failed"
+
+    move_from_tmp $tmppath $sourcepath
+
 fi
-
-
-
